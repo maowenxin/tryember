8826954cbf62388a65413fa53cb46c16e8612802
maowenxin <maowenxin72@yahoo.com>
2017-10-08 16:35:54 -0500

    try ember

---
+++ app/routes/orders.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    
+    model(){
+        return [
+            { id:'1', name:'Nate'},
+            { id:'2', name:'Gregg'}
+        ];
+    }
+});
